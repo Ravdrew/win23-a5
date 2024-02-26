426a12dda715e725f33f81ba40ea3722db4c14a4
Ravdrew <andresegarcia@me.com>
2024-02-25 19:51:13 -0800

    basic behavior tests

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -29,3 +29,21 @@ if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. succe
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
   exit 1
 fi
+
+# Test 04: Ensure simple case has correct output...
+if [[ $($CALCULATOR 2 / 4) -ne 0 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (2 / 4) failed to produce 0 as an output!'
+  exit 1
+fi
+
+# Test 04: Ensure simple case has correct output...
+if [[ $($CALCULATOR 2 - 4) -ne -2 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (2 - 4) failed to produce -2 as an output!'
+  exit 1
+fi
+
+# Test 05: Ensure simple case has correct output...
+if [[ $($CALCULATOR 2 * 4) -ne -2 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (2 * 4) failed to produce 8 as an output!'
+  exit 1
+fi
